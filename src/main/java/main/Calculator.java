94cd3ef4724f22af9460e742b26e08702a9792d5
sony <sony@DESKTOP-8UF28TN>
2019-03-29 12:03:53 +0530

    Completed adding, subtracting, multiplying and dividing two numbers.

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -2,14 +2,14 @@ package main;
 import java.util.*; 
 
 public class Calculator {
-    public static void main(String[] args)
+    private static Scanner inp;
+	public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        inp = new Scanner(System.in);
         int num1,num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
-        int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
@@ -38,26 +38,27 @@ public class Calculator {
     public static int add(int x, int y)
     {
     	int result=0;
-//        write your code here 
+    
+    	result =x+y;
     	
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
-//        write your code here
+        result =x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+       result =x*y;      
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
-//      write your code here
+        result =x/y;
     	return result;
     }
 
